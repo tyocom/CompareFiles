7cf86884d2eec4f09fdd49f9932e78d7573fd6a6
tyocom <tyocom@verizon.net>
2014-09-01 06:20:25 -0400

    Test Github

---
+++ CompareFiles/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace CompareLists
+{
+    partial class frmFileCompare
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFile1 = new System.Windows.Forms.TextBox();
+            this.txtFile2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cboField = new System.Windows.Forms.ComboBox();
+            this.lblField = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnSelectFile1 = new System.Windows.Forms.Button();
+            this.btnSelectFile2 = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSelectOutput = new System.Windows.Forms.Button();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.btnCompare2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtFile1
+            // 
+            this.txtFile1.Location = new System.Drawing.Point(120, 70);
+            this.txtFile1.Name = "txtFile1";
+            this.txtFile1.Size = new System.Drawing.Size(385, 20);
+            this.txtFile1.TabIndex = 0;
+            // 
+            // txtFile2
+            // 
+            this.txtFile2.Location = new System.Drawing.Point(120, 119);
+            this.txtFile2.Name = "txtFile2";
+            this.txtFile2.Size = new System.Drawing.Size(385, 20);
+            this.txtFile2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "File 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "File 2:";
+            // 
+            // cboField
+            // 
+            this.cboField.FormattingEnabled = true;
+            this.cboField.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.cboField.Location = new System.Drawing.Point(230, 244);
+            this.cboField.Name = "cboField";
+            this.cboField.Size = new System.Drawing.Size(121, 21);
+            this.cboField.TabIndex = 4;
+            // 
+            // lblField
+            // 
+            this.lblField.AutoSize = true;
+            this.lblField.Location = new System.Drawing.Point(161, 252);
+            this.lblField.Name = "lblField";
+            this.lblField.Size = new System.Drawing.Size(32, 13);
+            this.lblField.TabIndex = 5;
+            this.lblField.Text = "Field:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(190, 303);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Compare v1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnSelectFile1
+            // 
+            this.btnSelectFile1.Location = new System.Drawing.Point(512, 70);
+            this.btnSelectFile1.Name = "btnSelectFile1";
+            this.btnSelectFile1.Size = new System.Drawing.Size(30, 23);
+            this.btnSelectFile1.TabIndex = 7;
+            this.btnSelectFile1.Text = "...";
+            this.btnSelectFile1.UseVisualStyleBackColor = true;
+            this.btnSelectFile1.Click += new System.EventHandler(this.btnSelectFile1_Click);
+            // 
+            // btnSelectFile2
+            // 
+            this.btnSelectFile2.Location = new System.Drawing.Point(512, 122);
+            this.btnSelectFile2.Name = "btnSelectFile2";
+            this.btnSelectFile2.Size = new System.Drawing.Size(30, 23);
+            this.btnSelectFile2.TabIndex = 8;
+            this.btnSelectFile2.Text = "...";
+            this.btnSelectFile2.UseVisualStyleBackColor = true;
+            this.btnSelectFile2.Click += new System.EventHandler(this.btnSelectFile2_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(120, 176);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(385, 20);
+            this.txtOutput.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 176);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Output";
+            // 
+            // btnSelectOutput
+            // 
+            this.btnSelectOutput.Location = new System.Drawing.Point(512, 172);
+            this.btnSelectOutput.Name = "btnSelectOutput";
+            this.btnSelectOutput.Size = new System.Drawing.Size(30, 23);
+            this.btnSelectOutput.TabIndex = 11;
+            this.btnSelectOutput.Text = "..";
+            this.btnSelectOutput.UseVisualStyleBackColor = true;
+            this.btnSelectOutput.Click += new System.EventHandler(this.btnSelectOutput_Click);
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Location = new System.Drawing.Point(200, 18);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(166, 13);
+            this.lblDescription.TabIndex = 12;
+            this.lblDescription.Text = "Find what is in File 1 but not File 2";
+            // 
+            // btnCompare2
+            // 
+            this.btnCompare2.Location = new System.Drawing.Point(333, 303);
+            this.btnCompare2.Name = "btnCompare2";
+            this.btnCompare2.Size = new System.Drawing.Size(75, 23);
+            this.btnCompare2.TabIndex = 13;
+            this.btnCompare2.Text = "Compare v2";
+            this.btnCompare2.UseVisualStyleBackColor = true;
+            this.btnCompare2.Click += new System.EventHandler(this.btnCompare2_Click);
+            // 
+            // frmFileCompare
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(630, 430);
+            this.Controls.Add(this.btnCompare2);
+            this.Controls.Add(this.lblDescription);
+            this.Controls.Add(this.btnSelectOutput);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnSelectFile2);
+            this.Controls.Add(this.btnSelectFile1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblField);
+            this.Controls.Add(this.cboField);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFile2);
+            this.Controls.Add(this.txtFile1);
+            this.Name = "frmFileCompare";
+            this.Text = "Compare Files (Tab Delimited)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFile1;
+        private System.Windows.Forms.TextBox txtFile2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cboField;
+        private System.Windows.Forms.Label lblField;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnSelectFile1;
+        private System.Windows.Forms.Button btnSelectFile2;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSelectOutput;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.Button btnCompare2;
+    }
+}
+
---
+++ CompareFiles/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace CompareLists
+{
+    public partial class frmFileCompare : Form
+    {
+        public frmFileCompare()
+        {
+            InitializeComponent();
+        }
+
+      
+        private void btnSelectFile1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.ShowDialog();
+            txtFile1.Text = fd.FileName;
+        }
+
+        private void btnSelectFile2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.ShowDialog();
+            txtFile2.Text = fd.FileName;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<string> l1 = CreateList(txtFile1.Text);
+            List<string> l2 = CreateList(txtFile2.Text);
+
+            List<string> lout = CompareLists(l1, l2);
+
+            StreamWriter sw = new StreamWriter(txtOutput.Text);
+            foreach (string s in lout)
+                sw.WriteLine(s);
+            sw.Close();
+            MessageBox.Show("Finished");
+
+        }
+
+        private List<string> CompareLists (List<string> List1, List<string> list2)
+        {
+            List<string> result = new List<string>();
+
+            var q = from i in List1
+                    where (!list2.Contains(i))
+                    select i;
+
+            foreach (string s in q)
+                result.Add(s);
+
+            return result;
+        }
+
+        private List<string> CreateList(string filename)
+        {
+            StreamReader sr = new StreamReader(filename);
+            List<string> list = new List<string>();
+
+            int fieldnum = Convert.ToInt32 (cboField.Text);
+
+            while (sr.Peek() > -1)
+            {
+                string dataline = sr.ReadLine();
+                string[] flds = Regex.Split(dataline, "\t");
+                list.Add (flds[fieldnum]);
+            }
+            sr.Close();
+            return list;
+        }
+
+        private void btnSelectOutput_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sd = new SaveFileDialog();
+            sd.ShowDialog();
+            txtOutput.Text = sd.FileName;
+        }
+
+        private void btnCompare2_Click(object sender, EventArgs e)
+        {
+            List<string> thelist = CreateList(txtFile2.Text);
+            CompareFileToList(txtFile1.Text, thelist);
+        }
+
+        private void CompareFileToList (string filename, List<string> l)
+        {
+            StreamReader sr = new StreamReader(filename);
+
+            sr.Close();
+        }
+
+    }
+}
